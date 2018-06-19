5f433588675cac29548cebaa0c69d95c994889c6
Ulices Meléndez Acosta <ulices009@hotmail.com>
2018-06-18 22:16:32 -0500

    Add ArticleCell

--- CleanNews/Scenes/HeadlineList/HeadlineListVC.swift
+++ CleanNews/Scenes/HeadlineList/HeadlineListVC.swift
@@ -10,6 +10,9 @@ import UIKit
 import Lottie
 //#6DD88A
 
+// TODO: Create extension for reuse identifiers
+let cellRI = "ArticleCell"
+
 protocol HeadlineListDisplayLogic: class {
     func displayTopHeadlines(_ articles: [Article], total: Int)
     func displayErrorMessage(_ errorMessage: String)
@@ -20,16 +23,22 @@ protocol HeadlineListDisplayLogic: class {
 class HeadlineListVC: UIViewController, HeadlineListDisplayLogic {
 
     var interactor: HeadlineListBusinessLogic!
+    
     var articles: [Article] = []
     var currentPage = 1
     var totalArticles = 0
     var currentCategory = "business"
     
-    @IBOutlet weak var articlesTable: UITableView!
+    var articlesTable: UITableView = UITableView()
     
     override func awakeFromNib() {
         super.awakeFromNib()
         setup()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        //setupTableView()
     }
     
     override func viewDidLoad() {
@@ -51,8 +60,23 @@ class HeadlineListVC: UIViewController, HeadlineListDisplayLogic {
     }
     
     private func setupTableView() {
+        view.addSubview(articlesTable)
+        articlesTable.translatesAutoresizingMaskIntoConstraints = false
+        articlesTable.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        articlesTable.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        articlesTable.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        articlesTable.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        
+        articlesTable.tableFooterView = UIView(frame: .zero)
+        
         articlesTable.delegate = self
         articlesTable.dataSource = self
+        
+        articlesTable.estimatedRowHeight = 44
+        articlesTable.rowHeight = UITableViewAutomaticDimension
+        
+        articlesTable.register(ArticleCell.self, forCellReuseIdentifier: cellRI)
+        
     }
     
     func displayTopHeadlines(_ articles: [Article], total: Int) {
@@ -87,9 +111,18 @@ extension HeadlineListVC: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let article = articles[indexPath.row]
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellRI, for: indexPath) as! ArticleCell
+        cell.setupData(article: article)
+        return cell
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
     
 }
 
--- CleanNews/Views/List/ArticleCell.swift
+++ CleanNews/Views/List/ArticleCell.swift
@@ -9,16 +9,87 @@
 import UIKit
 
 class ArticleCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    private let titleTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = "Test label"
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.isEditable = false
+        textView.font = UIFont.boldSystemFont(ofSize: 17)
+        textView.textContainer.maximumNumberOfLines = 3
+        textView.textContainer.lineBreakMode = .byTruncatingTail
+        textView.textContainerInset = UIEdgeInsets.zero
+        textView.textContainer.lineFragmentPadding = 0
+        return textView
+    }()
+    
+    private let sourceLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor(named: "PrimaryColor")
+        label.font = UIFont.boldSystemFont(ofSize: 15)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let articleImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        autoresizesSubviews = true
+        setupView()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
+    
+    private func setupView() {
+        
+        let bottomContainer = UIView()
+        
+        contentView.addSubview(bottomContainer)
+        
+        contentView.addSubview(articleImageView)
+        contentView.addSubview(titleTextView)
+        bottomContainer.addSubview(sourceLabel)
+        
+        let marginGuide = contentView.layoutMarginsGuide
+        
+        articleImageView.topAnchor.constraint(equalTo: marginGuide.topAnchor, constant: 8).isActive = true
+        articleImageView.trailingAnchor.constraint(equalTo: marginGuide.trailingAnchor).isActive = true
+        articleImageView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        articleImageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        
+        titleTextView.topAnchor.constraint(equalTo: marginGuide.topAnchor, constant: 8).isActive = true
+        titleTextView.leadingAnchor.constraint(equalTo: marginGuide.leadingAnchor).isActive = true
+        titleTextView.trailingAnchor.constraint(equalTo: articleImageView.leadingAnchor, constant: -8).isActive = true
+        titleTextView.bottomAnchor.constraint(equalTo: bottomContainer.topAnchor, constant: -8).isActive = true
+        
+        bottomContainer.translatesAutoresizingMaskIntoConstraints = false
+        bottomContainer.topAnchor.constraint(equalTo: articleImageView.bottomAnchor, constant: 8).isActive = true
+        bottomContainer.leadingAnchor.constraint(equalTo: marginGuide.leadingAnchor).isActive = true
+        bottomContainer.trailingAnchor.constraint(equalTo: marginGuide.trailingAnchor).isActive = true
+        bottomContainer.bottomAnchor.constraint(equalTo: marginGuide.bottomAnchor).isActive = true
+        bottomContainer.heightAnchor.constraint(equalToConstant: 25).isActive = true
 
+        sourceLabel.topAnchor.constraint(equalTo: bottomContainer.topAnchor).isActive = true
+        sourceLabel.bottomAnchor.constraint(equalTo: bottomContainer.bottomAnchor).isActive = true
+        sourceLabel.leadingAnchor.constraint(equalTo: bottomContainer.leadingAnchor).isActive = true
+        sourceLabel.trailingAnchor.constraint(equalTo: bottomContainer.trailingAnchor).isActive = true
+        
+        // TODO: Add date next to sourceLabel
+        
+    }
+    
+    func setupData(article: Article) {
+        articleImageView.image = #imageLiteral(resourceName: "news_placeholder")
+        titleTextView.text = article.title ?? "gg"
+        sourceLabel.text = "BBC News"
+    }
+    
 }
